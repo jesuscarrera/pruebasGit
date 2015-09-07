a27d5e2e3af8f5e0732999e208d363a6c6c1e4fb
Jesus Carrera <jesus.carrera@masnegocio.com>
2015-09-07 14:07:15 -0500

    Primer php

---
+++ principal.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<title>Bienvenidos</title>
+	</head>
+	<body>
+		<h1>Primer contacto con GitHub</h1>
+		<h2>De Remoto a Local</h2>
+		<?php echo  "Hola Git Hub"; ?>
+	</body>
+</html>
